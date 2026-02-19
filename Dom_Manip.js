daabb9870e67184e573e9cfa8b8922f23452b277
Nick-Bonilla-0915 <ni865929@ucf.edu>
2026-02-19 16:36:42 -0500

    Update Dom_Manip.js

---
+++ Dom_Manip.js
@@ -0,0 +1,19 @@
+function showFilter()
+{
+    var x = document.getElementById("filterContent");
+    x[0].style.display = block;
+}
+
+function showAddNew(){
+    var y = document.getElementById("newContent");
+    y[0].style.display = block;
+}
+
+function addNewArticle(){
+
+}
+
+function filterArticles(){
+
+}
+
