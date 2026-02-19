332ab5eceedd586fe5833dd5084f15d935e09f54
Nick-Bonilla-0915 <ni865929@ucf.edu>
2026-02-19 16:44:00 -0500

    Trying to fix buttons

--- Dom_Manip.js
+++ Dom_Manip.js
@@ -1,11 +1,15 @@
 function showFilter()
 {
     var x = document.getElementById("filterContent");
+    var y = document.getElementById("newContent");
     x[0].style.display = block;
+    y[0].style.display = none;
 }
 
 function showAddNew(){
+    var x = document.getElementById("filterContent");
     var y = document.getElementById("newContent");
+    x[0].style.display = none;
     y[0].style.display = block;
 }
 
