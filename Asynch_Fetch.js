66b4a854829dc2e3671140af55803f349b166fce
Nick-Bonilla-0915 <ni865929@ucf.edu>
2026-03-03 20:44:11 -0500

    Update Asynch_Fetch.js

--- Asynch_Fetch.js
+++ Asynch_Fetch.js
@@ -1,19 +1,29 @@
-function findPokemon(){
-    const pokeID = document.getElementById("inName").value.toLowerCase();
+async function findPokemon(){
+    try{
+        //Taking name or ID then fetching data from api
+        const pokeID = document.getElementById("inName").value.toLowerCase();
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}`)
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}`)
-        .then(response => {
-            if(!response.ok){
-                throw new Error("Pokemon Not Found");
-            }
-            return response.json();
-        })
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-    
+        //Checking if fetch is successful
+        if(!Response.ok)
+        {
+            throw new Error("Pokemon Not Found");
+        }
+
+        //Receiving data
+        const data = await response.json();
+
+        //Updating Pokemon sprite
         const pokeSprite = data.sprites.front_default;
         const spriteShow = document.getElementById("pokeImg");
         spriteShow.src = pokeSprite;
+    }
+    catch(error)
+    {
+        console.error(error);
+    }
+    
+        
 }
 
 function addPokemon(){
