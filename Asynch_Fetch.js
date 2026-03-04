94750c9192dafd12ab9cf03383c235445106a6e2
Nick-Bonilla-0915 <ni865929@ucf.edu>
2026-03-03 19:45:38 -0500

    Update Asynch_Fetch.js

--- Asynch_Fetch.js
+++ Asynch_Fetch.js
@@ -1,5 +1,5 @@
 function findPokemon(){
-    fetch('https://pokeapi.co/api/v2/5/')
+    fetch('https://pokeapi.co/api/v2/pokemon/5')
         .then(res => res.json())
         .then(data => console.log(data));
 }
